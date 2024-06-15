8e2f9cc0fd7ffe81b07de5c1da5a7cde4e7f55c5
jericho909 <omerahmetyucel@gmail.com>
2024-06-15 18:47:08 +0300

    edits

--- src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
+++ src/main/java/simsek/ali/VeterinaryManagementProject/core/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("...")
+                .allowedOrigins("https://vetapp-ten.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
